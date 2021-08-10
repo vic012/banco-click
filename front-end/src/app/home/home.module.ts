9331cd234ac780a0526f7f93f772f9f4942db880
Felipe Henrique Gross Windmoller <felipewind@gmail.com>
2021-08-10 13:55:24 -0300

    versao inicial

---
+++ front-end/src/app/home/home.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SharedModule } from './../shared/shared.module';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule
+  ]
+})
+export class HomeModule { }
